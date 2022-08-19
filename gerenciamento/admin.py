357019fd698c0be71c360bf10346591f05e0d21a
Juliana Macedo <juliana.smacedo@b2wdigital.com>
2022-08-19 15:18:27 -0300

    feat[OTS-559]:alterações model

--- gerenciamento/admin.py
+++ gerenciamento/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Dados_Status, Dados
+from .models import Dados_Status
 
 
-admin.site.register(Dados_Status, Dados)
+admin.site.register(Dados_Status)
 
 # Register your models here.
